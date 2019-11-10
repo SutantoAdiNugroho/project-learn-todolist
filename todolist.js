e5b531113ba8d28ce38eb4aeae6d513502232d9e
SutantoAdiNugroho <adi.nugroho.an836@gmail.com>
2019-11-10 15:43:57 +0700

    add bootstrap on index

--- todolist.js
+++ todolist.js
@@ -1,19 +1,80 @@
-var myTodos = ["Makan", "Minum"];                               
+//DOM TodoList
+var inputText = document.getElementById("txt"),
+items = document.getElementsByClassName("list-group-item"),
+tab = [], index;
 
-var txtTodo = ('My Todo : ' + myTodos.join(', '));
+for (var i = 0; i < items.length; i++){
+    tab.push(items[i].innerHTML);
+}
 
-if (confirm(`${txtTodo}\n\nTambahkan data?`)) {
-    for (var i = 0; i < 1; i++) {             
-        myTodos.push(prompt('Tambahkan Todo ' + (i))); 
-    }
-} else {
-    if (confirm(`Hapus data?`)) {
-        for (var i = 0; i < 1; i++) {             
-            myTodos.pop(prompt('Hapus Todo ' + (i))); 
-        }
-    } else {
-
+for (var i = 0; i < items.length; i++){
+    items[i].onclick = function(){
+        index = tab.indexOf(this.innerHTML);
+        inputText.value = this.innerHTML; 
     }
 }
 
-alert('My Todo : ' + myTodos.join(', ')); 
+function changeColorList(color) { 
+    // document.body.style.background = color; 
+    
+} 
+
+function refreshArray() {
+    tab.length = 0;
+    items = document.getElementsByClassName("list-group-item");
+    for(var i = 0; i < items.length; i++){
+        tab.push(items[i].innerHTML);
+    }
+}
+
+function addLI(){
+    var listNode = document.getElementById("list"), textNode = document.createTextNode(inputText.value), liNode = document.createElement("li");
+    liNode.className = "list-group-item"
+    
+    liNode.appendChild(textNode);
+    listNode.appendChild(liNode);
+    
+    refreshArray();
+    
+    liNode.onclick = function(){
+        index = tab.indexOf(liNode.innerHTML);
+        inputText.value = liNode.innerHTML;
+    }
+}
+
+function editLI(){
+    items[index].innerHTML = inputText.value;
+    refreshArray();
+}
+
+function deleteLI(){
+    refreshArray();
+    if(items.length > 0){
+         items[index].parentNode.removeChild(items[index]);
+         inputText.value = "";
+     }
+}
+
+
+
+//CONSOLE TodoList
+
+// var myTodos = ["Makan", "Minum"];                               
+
+// var txtTodo = ('My Todo : \n'+ myTodos.join('\n'));
+
+// if (confirm(`${txtTodo}\n\nTambahkan data?`)) {
+//     for (var i = 0; i < 1; i++) {             
+//         myTodos.push(prompt('Tambahkan Todo ' + (i))); 
+//     }
+// } else {
+//     if (confirm(`Hapus data?`)) {
+//         for (var i = 0; i < 1; i++) {             
+//             myTodos.splice(prompt('Hapus Todo ' + (i))); 
+//         }
+//     } else {
+
+//     }
+// }
+
+// alert('My Todo : \n' + myTodos.join('\n')); 
